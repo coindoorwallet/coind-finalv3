8d76ebf2ba43b318043ca1c5bec1f503102a798e
coindoorwallet <coindoorwallet@gmail.com>
2025-11-25 23:52:04 +0200

    Create Footer.js

---
+++ app/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full p-6 mt-10 border-t border-neutral-800 text-center text-neutral-400">
+      <p>© {new Date().getFullYear()} CoinDoor. All rights reserved.</p>
+    </footer>
+  );
+}
