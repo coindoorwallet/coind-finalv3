db17b884ad6e1aa0e7bcdb61592d5031dd3606ac
coindoorwallet <coindoorwallet@gmail.com>
2025-11-25 22:15:44 +0200

    In wrong path

--- components/Navbar.js
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="w-full p-4 border-b border-neutral-800 flex justify-between items-center">
-      <h1 className="text-xl font-bold">CoinDoor</h1>
-    </nav>
-  );
-}
