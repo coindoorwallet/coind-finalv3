ccf86e601b196ac5ee6daac9e825b3dc007e5cb0
coindoorwallet <coindoorwallet@gmail.com>
2025-11-25 21:42:59 +0200

    Create page.js

---
+++ app/markets/page.js
@@ -0,0 +1,8 @@
+export default function MarketsPage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold mb-4">Markets</h1>
+      <p>Charts coming soon.</p>
+    </main>
+  );
+}
