205978a33ff601a50f318ba6a7777f8aa90b1f9e
coindoorwallet <coindoorwallet@gmail.com>
2025-11-25 20:56:40 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-10 text-center">
+      <h1 className="text-4xl font-bold">CoinDoor</h1>
+      <p className="mt-4 text-lg opacity-60">Live crypto charts & news</p>
+    </main>
+  );
+}
